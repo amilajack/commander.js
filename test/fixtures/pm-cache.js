9249b77e7967b4738ff08b8e3ae72250ffdbce3a
Amila Welihinda <amilajack@gmail.com>
2019-03-03 00:33:55 -0800

    update tests to reflect api changes

--- test/fixtures/pm-cache.js
+++ test/fixtures/pm-cache.js
@@ -1,6 +1,6 @@
 import program from '../../src';
 
-program
+program()
   .command('clear', 'clear the cache')
   .command('validate', 'validate the cache', { isDefault: true })
   .parse(process.argv);
